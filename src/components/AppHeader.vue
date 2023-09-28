f9637c98eba279df87cbae346b5df99756fec9f2
BuiKiet <roiyano1utl@gmail.com>
2023-09-28 22:07:56 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <router-link to="/" class="navbar-brand">Ứng dụng Quản lý danh bạ</router-link>
+    <div class="mr-auto navbar-nav">
+      <li class="nav-item">
+        <router-link :to="{ name: 'contactbook' }" class="nav-link">
+          Danh bạ
+          <i class="fas fa-address-book"></i>
+        </router-link>
+      </li>
+    </div>
+  </nav>
+</template>
